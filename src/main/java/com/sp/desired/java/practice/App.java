1f8eb95fe32950faab0a6c804ab347e49a0e4faa
pottipadusahi <Sahith.Pottipadu@garmin.com>
2023-11-21 11:27:47 -0600

    initial commit for adding a maven project

---
+++ src/main/java/com/sp/desired/java/practice/App.java
@@ -0,0 +1,13 @@
+package com.sp.desired.java.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
